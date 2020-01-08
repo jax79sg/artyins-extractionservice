c11c13ec871018fdea79ee648283b7a2206ff1e1
jax79sg <jax79sg@yahoo.com.sg>
2020-01-08 17:41:44 +0800

    Added md5 for tika server

--- setup_prerequisites.sh
+++ setup_prerequisites.sh
@@ -6,8 +6,10 @@ if test -f "tika-server.jar"; then
 else
    echo "tika-server.jar does not exists, downloading now"
    wget -O tika-server.jar http://search.maven.org/remotecontent?filepath=org/apache/tika/tika-server/1.23/tika-server-1.23.jar
+   wget -O tika-server.jar.md5 http://search.maven.org/remotecontent?filepath=org/apache/tika/tika-server/1.23/tika-server-1.23.jar.md5
 fi
 mkdir /tmp
 cp tika-server.jar /tmp/
+cp tika-server.jar.md5 /tmp/
 echo "Installing python requirements"
 pip install -r requirements.txt
