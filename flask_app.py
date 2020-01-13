5a323499a9a323efb2cce0f6c77f6656ac05d930
jax79sg <jax79sg@yahoo.com.sg>
2020-01-13 13:01:16 +0800

    include stdout loggging

--- flask_app.py
+++ flask_app.py
@@ -21,7 +21,10 @@ ROOT_DIR = os.path.abspath("../")
 sys.path.append(ROOT_DIR)  # To find local version of the library
 
 # Logging confg
-logging.basicConfig(level=logging.DEBUG, filename="log", filemode="a+",
+logging.basicConfig(level=logging.DEBUG,handlers=[
+        logging.FileHandler("{0}/{1}.log".format(".", "log")),
+        logging.StreamHandler()
+    ],
                 format="%(asctime)-15s %(levelname)-8s %(message)s")
 
 ############################################################
