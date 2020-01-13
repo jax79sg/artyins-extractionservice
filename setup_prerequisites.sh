ada7ce611051f5db9d1a980e920fbe1e0e8d3712
jax79sg <jax79sg@yahoo.com.sg>
2020-01-13 13:08:46 +0800

    change wget to axel

--- setup_prerequisites.sh
+++ setup_prerequisites.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 echo on
-export TIKA_SERVER_JAR=$PWD/tika-server.jar
-if test -f "tika-server.jar"; then
+export TIKA_SERVER_JAR=/tmp/tika-server.jar
+mkdir /tmp
+if test -f "/tmp/tika-server.jar"; then
    echo "tika-server.jar exists"
 else
    echo "tika-server.jar does not exists, downloading now"
-   wget -O tika-server.jar http://search.maven.org/remotecontent?filepath=org/apache/tika/tika-server/1.23/tika-server-1.23.jar
-   wget -O tika-server.jar.md5 http://search.maven.org/remotecontent?filepath=org/apache/tika/tika-server/1.23/tika-server-1.23.jar.md5
+   axel -n 5 -o /tmp/tika-server.jar  http://search.maven.org/remotecontent?filepath=org/apache/tika/tika-server/1.23/tika-server-1.23.jar
+   axel -n 5 -o /tmp/tika-server.jar.md5 http://search.maven.org/remotecontent?filepath=org/apache/tika/tika-server/1.23/tika-server-1.23.jar.md5
 fi
-mkdir /tmp
-cp tika-server.jar /tmp/
-cp tika-server.jar.md5 /tmp/
 mkdir /mnt
 mkdir /mnt/shareddata
 cp test.pdf /mnt/shareddata
