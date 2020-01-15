882b30cad79047e967362868fd335cc1390abb8d
jax79sg <jax79sg@yahoo.com.sg>
2020-01-15 11:17:50 +0800

    update

--- flask_app.py
+++ flask_app.py
@@ -22,7 +22,7 @@ sys.path.append(ROOT_DIR)  # To find local version of the library
 
 # Logging confg
 logging.basicConfig(level=logging.DEBUG,handlers=[
-        logging.FileHandler("{0}/{1}.log".format("/logs", "extractionservice")),
+        logging.FileHandler("{0}/{1}.log".format("/logs", "extractionservice-flaskapp")),
         logging.StreamHandler()
     ],
                 format="%(asctime)-15s %(levelname)-8s %(message)s")
