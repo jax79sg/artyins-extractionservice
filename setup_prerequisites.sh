a94c725cc4d92f82dcda834b4c2f833ad2f3b2d1
jax79sg <jax79sg@yahoo.com.sg>
2020-01-17 10:21:39 +0800

    update

--- setup_prerequisites.sh
+++ setup_prerequisites.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 echo on
 export TIKA_SERVER_JAR=/tmp/tika-server.jar
+sudo apt update
+sudo apt install -y axel
 mkdir /tmp
 rm -r /tmp/tika*
 axel -o /tmp/tika-server.jar -n 10  http://search.maven.org/remotecontent?filepath=org/apache/tika/tika-server/1.23/tika-server-1.23.jar
