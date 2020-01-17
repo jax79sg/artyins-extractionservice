efa21094390c8accf123ead29171c3d4fd719e0a
jax79sg <jax79sg@yahoo.com.sg>
2020-01-17 10:18:03 +0800

    update

--- rebuild.sh
+++ rebuild.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 git commit -a -m update
 git push
-docker build ./docker/. --no-cache -t artyins-extractionservice
+docker build ./docker/. --no-cache -t artyins-extractionservice:1.0-SNAPSHOT
